c19ac9d8c42e48ee750e24ce70a90238ef387868
Nils Hamerlinck <nils@trobz.com>
2021-12-23 12:24:06 +0700

    [IMP] mass_editing: replace test on res.users.log_ids by a test on res.partner.child_ids (these log_ids records are system records, better test on normal records)

--- mass_editing/models/__init__.py
+++ mass_editing/models/__init__.py
@@ -1,3 +1,2 @@
 from . import ir_actions_server
 from . import mass_editing_line
-from . import res_users_log
--- mass_editing/models/res_users_log.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ResUsersLog(models.Model):
-    _inherit = "res.users.log"
-
-    create_uid = fields.Many2one("res.users", ondelete="cascade")
